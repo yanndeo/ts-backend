4b9b812e2d749f2f7f99f1fdbe63279d7d32cfdd
yanndeo <yan2sambou@gmail.com>
2026-01-14 23:04:15 +0100

    wip:(policy) :add global permission and contextuel permission

--- src/policies/UserPolicy.ts
+++ src/policies/UserPolicy.ts
@@ -1,12 +1,40 @@
+import { Permission } from '../permissions/index';
+import { ROLE_PERMISSIONS } from '../roles';
+
+
+type AuthUser = {
+    userId: number;
+    role: "USER" | "ADMIN";
+};
 
 
 export class UserPolicy {
 
-    static canDeleteUser(authUser: { userId: number; isAdmin: boolean }, targetUserId: number): boolean {
+    /* static canDeleteUser(authUser: { userId: number; isAdmin: boolean }, targetUserId: number): boolean {
         if (authUser.isAdmin) {
             return true;
         }
 
         return authUser.userId === targetUserId;
+    } */
+
+    static can(
+        user: AuthUser,
+        permission: Permission,
+        targetUserId: number
+    ): boolean {
+
+        const permissions = ROLE_PERMISSIONS[user.role];
+
+        if (!permissions.includes(permission)) {
+            return false;
+        }
+
+        if (permission === Permission.DELETE_USER) {
+            return user.userId === targetUserId;
+        }
+
+        return true;
     }
+
 }
