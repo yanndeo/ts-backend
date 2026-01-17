fffe061463990b0a553a6d8f577dd8349ebe0782
yanndeo <yan2sambou@gmail.com>
2026-01-18 00:05:27 +0100

    wip: email value object

---
+++ src/values-objects/Email.ts
@@ -0,0 +1,32 @@
+
+class Email {
+
+    private readonly value: string;
+
+    private constructor(value: string) {
+        this.value = value;
+    }
+
+
+    static create(email: string): Email {
+        const normalized = email.trim().toLowerCase();
+
+        if (! Email.isValid(normalized)) {
+            throw new Error("Invalid email");
+        }
+
+        return new Email(normalized);
+    }
+
+
+    getValue(): string {
+        return this.value;
+    }
+
+
+    static isValid(email: string): boolean {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        return emailRegex.test(email);
+    }
+}
